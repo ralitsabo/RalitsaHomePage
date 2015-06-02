e9937cf52b4ee83f6d54264f858cdb1b3c56dfe6
Ralitsa Borisova <ralitsa.bb@gmail.com>
2015-06-02 18:00:43 +0300

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+# class ApplicationController < ActionController
+#   def index
+#      redirect_to :action => :contact
+#   end
+
+#   def contact
+#   end
+# end
