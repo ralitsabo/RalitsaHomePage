da86ab953c190af10a0d4e15b627aabe019bb0ce
Ralitsa Borisova <RalitsaBorisova@EMP-SOF-LT097.local>
2016-10-20 13:54:28 +0300

    fix tests ?=

--- spec/controllers/application_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'rails_helper'
-
-describe ApplicationContoller, :type => :controller do
-  it "renders the index template" do
-    get :index
-    expect(response).to render_template("index")
-  end
-end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,9 +1,5 @@
-require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe ApplicationController do
-  it "responds successfully" do
-    get :index
-    expect(response.status).to eq(200)
-  end
+RSpec.describe WelcomeController, type: :controller do
+
 end
